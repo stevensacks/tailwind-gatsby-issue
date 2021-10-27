dde2894415e5948ab1b6046e2ad77f98692ba100
Steven Sacks <stevensacks@gmail.com>
2021-10-27 17:10:15 +0900

    include tailwind forms demo code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
 
 const options = [
-    {label: 'Item 1', value: '1'},
-    {label: 'Item 2', value: '2'},
+    {label: 'Corporate event', value: '1'},
+    {label: 'Wedding', value: '2'},
+    {label: 'Birthday', value: '3'},
+    {label: 'Other', value: '4'},
 ];
 
 const IndexPage = () => {
@@ -15,19 +17,40 @@ const IndexPage = () => {
     return(
         <div className="p-4">
             <h1 className="pb-2 text-2xl text-gray-900">Tailwind Forms Select Issue</h1>
-            <select
-                onChange={onChange}
-                value={state}
-            >
-                {options.map((item) => (
-                    <option
-                        key={String(item.value)}
-                        value={String(item.value)}
-                    >
-                        {item.label}
-                    </option>
-                ))}
-            </select>
+            <div className="w-1/4">
+                <div className="p-4 border">
+                    <h2>Copied from tailwindcss/forms demo source code</h2>
+                    <label className="block mt-2">
+                        <span className="text-gray-700">What type of event is it?</span>
+                        <select className="block w-full mt-1">
+                            <option>Corporate event</option>
+                            <option>Wedding</option>
+                            <option>Birthday</option>
+                            <option>Other</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="p-4 mt-4 border">
+                    <h2>React controlled select similar to demo</h2>
+                    <label className="block mt-2">
+                        <span className="text-gray-700">What type of event is it?</span>
+                        <select
+                            className="block w-full mt-1"
+                            onChange={onChange}
+                            value={state}
+                        >
+                            {options.map((item) => (
+                                <option
+                                    key={String(item.value)}
+                                    value={String(item.value)}
+                                >
+                                    {item.label}
+                                </option>
+                            ))}
+                        </select>
+                    </label>
+                </div>
+            </div>
         </div>
     );
 }
